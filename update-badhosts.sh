b0a14c3f1b6007b5c6b8e9cc77f97caa2d45850b
Bert Peters <bert.ljpeters@gmail.com>
2016-06-22 09:53:15 +0200

    Be quiet when not updating anything.

--- update-badhosts.sh
+++ update-badhosts.sh
@@ -13,9 +13,11 @@ cut -d '|'  -f 4 /var/db/sshguard/blacklist.db >> "$temp"
 sort -u "$temp" > "$badhosts"
 rm "$temp"
 
-if ! git diff-index --quiet HEAD
+if [ $(git ls-files -m | wc -l) -gt 0 ]
 then
 	git commit -am "Update bad host file at $(date "+%Y-%m-%d %H:%M:%S")"
 fi
 
-git pull -r && git push
+if [ "$(git rev-parse origin/master)" != "$(git rev-parse master)" ]; then
+	git pull -r && git push
+fi
